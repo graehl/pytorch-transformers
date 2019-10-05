96677a9c594a9383bba7598f9fab7131ef051699
graehl <graehl@gmail.com>
2019-10-05 13:01:44 -0700

    imdb

--- examples/run_glue.py
+++ examples/run_glue.py
@@ -542,7 +542,7 @@ def main():
                 result = dict((k + '_{}'.format(global_step), v) for k, v in result.items())
                 results.update(result)
             if args.server:
-
+                pass
 
     return results
 
