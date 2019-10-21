bbabc5f9332e5e20157a9fc24e29f960703269fb
graehl <graehl@gmail.com>
2019-10-20 22:14:44 -0700

    sentiment3 glue task

--- transformers/data/processors/glue.py
+++ transformers/data/processors/glue.py
@@ -318,6 +318,43 @@ class Sst2Processor(DataProcessor):
         return examples
 
 
+class Sentiment3Processor(DataProcessor):
+    """Processor for the SST-2 data set (GLUE version)."""
+
+    def get_example_from_tensor_dict(self, tensor_dict):
+        """See base class."""
+        return InputExample(tensor_dict['idx'].numpy(),
+                            tensor_dict['sentence'].numpy().decode('utf-8'),
+                            None,
+                            str(tensor_dict['label'].numpy()))
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_labels(self):
+        """See base class."""
+        return ["0", "1"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            guid = "%s-%s" % (set_type, i)
+            assert len(line) == 2, '%s %s' % (guid, line)
+            text_a = line[0]
+            label = line[1]
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
+
+
 class StsbProcessor(DataProcessor):
     """Processor for the STS-B data set (GLUE version)."""
 
@@ -527,7 +564,7 @@ glue_tasks_num_labels = {
     "rte": 2,
     "wnli": 2,
     "imdb": 2,
-    "sentiment": 2,
+    "sentiment3": 3,
 }
 
 glue_processors = {
@@ -542,7 +579,7 @@ glue_processors = {
     "rte": RteProcessor,
     "wnli": WnliProcessor,
     "imdb": Sst2Processor,
-    "sentiment": Sst2Processor,
+    "sentiment3": Sentiment3Processor,
 }
 
 glue_output_modes = {
@@ -557,5 +594,5 @@ glue_output_modes = {
     "rte": "classification",
     "wnli": "classification",
     "imdb": "classification",
-    "sentiment": "classification",
+    "sentiment3": "classification",
 }
