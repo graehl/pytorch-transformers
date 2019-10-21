c1b8e6147b43b4f3e18b8f56c62c9d6d32616894
graehl <graehl@gmail.com>
2019-10-21 00:32:24 -0700

    acc

--- transformers/data/metrics/__init__.py
+++ transformers/data/metrics/__init__.py
@@ -61,25 +61,11 @@ if _has_sklearn:
         assert len(preds) == len(labels)
         if task_name == "cola":
             return {"mcc": matthews_corrcoef(labels, preds)}
-        elif task_name == "sst-2":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "imdb":
-            return {"acc": simple_accuracy(preds, labels)}
         elif task_name == "mrpc":
+            return acc_and_f1(preds, labels)
+        elif task_name == "qqp":
             return acc_and_f1(preds, labels)
         elif task_name == "sts-b":
             return pearson_and_spearman(preds, labels)
-        elif task_name == "qqp":
-            return acc_and_f1(preds, labels)
-        elif task_name == "mnli":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "mnli-mm":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "qnli":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "rte":
-            return {"acc": simple_accuracy(preds, labels)}
-        elif task_name == "wnli":
-            return {"acc": simple_accuracy(preds, labels)}
         else:
-            raise KeyError(task_name)
+            return {"acc": simple_accuracy(preds, labels)}
