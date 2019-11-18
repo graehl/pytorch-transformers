f635789a3388cad0b84f62e84d007b59f8603c5a
graehl <graehl@gmail.com>
2019-11-17 19:18:36 -0800

    inputs

--- examples/run_glue.py
+++ examples/run_glue.py
@@ -335,19 +335,12 @@ def evaluate(args, model, tokenizer, prefix="", verbose=1):
         out_label_ids = None
         for batch in tqdm(eval_dataloader, desc="Evaluating", mininterval=mininterval):
             model.eval()
-            batch = tuple(t.to(args.device) for t in batch)
+            batch = tuple(t.to(args.device) for t in batch) # t[0] pair with input
             inputs = batch_inputs(batch, args.model_type)
             with torch.no_grad():
-                labels = batch[3]
-                input_ids = batch[0]
-                inputs = {'input_ids':      input_ids,
-                          'attention_mask': batch[1],
-                          'labels':         labels}
-                if args.model_type != 'distilbert':
-                    inputs['token_type_ids'] = batch[2] if args.model_type in ['bert', 'xlnet'] else None  # XLM, DistilBERT and RoBERTa don't use segment_ids
                 outputs = model(**inputs)
                 tmp_eval_loss, logits = outputs[:2]
-                outverbose('%s\t%s' % (rounded(logits.tolist()), labels.tolist()), v=1, seq=nb_eval_steps)
+                outverbose('%s\t%s' % (rounded(logits.tolist()), inputs['labels'].tolist()), v=1, seq=nb_eval_steps)
                 eval_loss += tmp_eval_loss.mean().item()
             nb_eval_steps += 1
             if preds is None:
