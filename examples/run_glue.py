adc89c4c4d93077a3f3de4f932741b9874de326e
graehl <graehl@gmail.com>
2019-11-17 22:27:22 -0800

    show-inputs

--- examples/run_glue.py
+++ examples/run_glue.py
@@ -470,7 +470,7 @@ def load_and_cache_examples(args, task, tokenizer, evaluate=False):
         logger.info("Loading features from cached file %s", cached_features_file)
         features = torch.load(cached_features_file)
         logger.info("Loading examples from cached file %s", cached_examples_file)
-        examples = [transformers.data.processors.utils.InputExample(x[0], x[1], x[2], x[3]) for x in load_tsv(cached_examples_file)]
+        examples = [data.processors.utils.InputExample(x[0], x[1], x[2], x[3]) for x in load_tsv(cached_examples_file)]
     else:
         logger.info("Creating features from dataset file at %s", args.data_dir)
         label_list = processor.get_labels()
